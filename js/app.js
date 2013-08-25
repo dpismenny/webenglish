d5bf3727d364d506b7fb73e7de54553163f38842
Alexander Burtsev <albburtsev@gmail.com>
2013-08-26 00:41:09 +0400

    audit.html layout fixes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 jQuery(function($) {
 	'use strict';
 
-	console.log('Do it!');
+	$('.js-audit-row').each(function() {
+		var	_row = $(this);
+
+		_row
+			.on('click', '.js-button-audit', function() {
+				_row.addClass('audit__row_long');
+				// @todo
+				return false;
+			})
+			.on('click', '.js-button-cancel', function() {
+				_row.removeClass('audit__row_long');
+				// @todo
+				return false;
+			});
+		
+	});
 });
